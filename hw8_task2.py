9cc9b6f490d03ccabb941327adfc9c3a27c27307
Yuliya-Ausiannikava <ovsannikova.y@gmail.com>
2026-03-16 23:31:17 +0300

    I created a file with code to complete the Infinite loop task

---
+++ hw8_task2.py
@@ -0,0 +1,14 @@
+"""
+This code solves the second task from homework #8 "Infinity loop"
+"""
+
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+while a != b:
+    a +=1
+    b -= 1
+    if a == b:
+        print(False)
+    if a > b:
+        print(True)
+        break
