57cbd009a8d78d165bd1c9ba83a528e29d978e66
Yuliya-Ausiannikava <ovsannikova.y@gmail.com>
2026-03-18 15:06:35 +0300

    I created a file with code to solve the Statues task

---
+++ hw8_task3.py
@@ -0,0 +1,12 @@
+"""
+This code solves the second task from homework #8 "Statues"
+"""
+
+size_1 = [3, 1, 8, 5, 10]
+size_2 = []
+
+for i in range (0, max(size_1)):
+    i += 1
+    if i not in size_1:
+        size_2.append(i)
+print(len(size_2))
