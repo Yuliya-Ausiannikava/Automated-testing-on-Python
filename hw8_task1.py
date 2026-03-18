94a6617a02cacce11ab546cb0e66016669a14222
Yuliya-Ausiannikava <ovsannikova.y@gmail.com>
2026-03-18 16:18:46 +0300

    Final fix at the request of linters

--- hw8_task1.py
+++ hw8_task1.py
@@ -5,28 +5,28 @@ This code solves the second task from homework #7 "Bulls and cows"
 import random
 
 while True:
-    number = str(random.randint(1000, 9999))
-    if len(set(number)) == 4:
+    NUMBER = str(random.randint(1000, 9999))
+    if len(set(NUMBER)) == 4:
         break
 
 while True:
-    guess = str(input('The number has been chosen. Try to guess it: '))
-    if len(guess) != 4:
+    GUESS = str(input('The number has been chosen. Try to guess it: '))
+    if len(GUESS) != 4:
         print('Please enter a 4 digit number:')
         continue
 
-    bulls = 0
-    cows = 0
+    BULLS = 0
+    COWS = 0
     for char in range(4):
-        if guess[char] == number[char]:
-            bulls += 1
+        if GUESS[char] == NUMBER[char]:
+            BULLS += 1
 
     for char in range(4):
-        if guess[char] in number and guess[char] != number[char]:
-            cows += 1
+        if GUESS[char] in NUMBER and GUESS[char] != NUMBER[char]:
+            COWS += 1
 
-    print(f'Bulles: {bulls},cows: {cows}')
+    print(f'Bulles: {BULLS},cows: {COWS}')
 
-    if bulls == 4:
+    if BULLS == 4:
         print('You win!')
         break
