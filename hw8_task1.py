ada5c76d7d8c0161d1d03651dd101c3b50ed743a
Yuliya-Ausiannikava <ovsannikova.y@gmail.com>
2026-03-18 16:13:32 +0300

    Fixed linter errors again

--- hw8_task1.py
+++ hw8_task1.py
@@ -5,24 +5,24 @@ This code solves the second task from homework #7 "Bulls and cows"
 import random
 
 while True:
-    NUMB = str(random.randint(1000, 9999))
-    if (NUMB[0] != NUMB[1] and NUMB[0] != NUMB[2] and NUMB[0] != NUMB[3]
-        and NUMB[1] != NUMB[2] and NUMB[1] != NUMB[3] and NUMB[2] != NUMB[3]):
+    number = str(random.randint(1000, 9999))
+    if len(set(number)) == 4:
         break
+
 while True:
-    guess_numb = str(input('The number has been chosen. Try to guess it: '))
-    if len(guess_numb) != 4:
+    guess = str(input('The number has been chosen. Try to guess it: '))
+    if len(guess) != 4:
         print('Please enter a 4 digit number:')
         continue
 
     bulls = 0
     cows = 0
     for char in range(4):
-        if guess_numb[char] == NUMB[char]:
+        if guess[char] == number[char]:
             bulls += 1
 
     for char in range(4):
-        if guess_numb[char] in NUMB and guess_numb[char] != NUMB[char]:
+        if guess[char] in number and guess[char] != number[char]:
             cows += 1
 
     print(f'Bulles: {bulls},cows: {cows}')
