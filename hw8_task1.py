0d24ebe7135367c6b7b370060471e04e0b8b0893
Yuliya-Ausiannikava <ovsannikova.y@gmail.com>
2026-03-18 15:59:26 +0300

    Fixed linter errors

--- hw8_task1.py
+++ hw8_task1.py
@@ -5,36 +5,28 @@ This code solves the second task from homework #7 "Bulls and cows"
 import random
 
 while True:
-    numb = str(random.randint(1000, 9999))
-    if numb[0] != numb[1] and numb[0] != numb[2] and numb[0] != numb[3] and numb[1] != numb[2] and numb[1] != numb[3] and numb[2] != numb[3]:
+    NUMB = str(random.randint(1000, 9999))
+    if (NUMB[0] != NUMB[1] and NUMB[0] != NUMB[2] and NUMB[0] != NUMB[3]
+        and NUMB[1] != NUMB[2] and NUMB[1] != NUMB[3] and NUMB[2] != NUMB[3]):
         break
 while True:
-    i = str(input('The number has been chosen. Try to guess it: '))
-    if len(i) != 4:
+    guess_numb = str(input('The number has been chosen. Try to guess it: '))
+    if len(guess_numb) != 4:
         print('Please enter a 4 digit number:')
         continue
+
     bulls = 0
     cows = 0
-    if numb[0] == i[0]:
-        bulls += 1
-    if numb[1] == i[1]:
-        bulls += 1
-    if numb[2] == i[2]:
-        bulls += 1
-    if numb[3] == i[3]:
-        bulls += 1
-    if i[0] == numb[1] or i[0] == numb[2] or i[0] == numb[3]:
-        cows += 1
-    if i[1] == numb[0] or i[1] == numb[2] or i[1] == numb[3]:
-        cows += 1
-    if i[2] == numb[0] or i[2] == numb[1] or i[2] == numb[3]:
-        cows += 1
-    if i[3] == numb[0] or i[3] == numb[1] or i[3] == numb[2]:
-        cows += 1
-    print(f'Bulles:{bulls},cows:{cows}')
+    for char in range(4):
+        if guess_numb[char] == NUMB[char]:
+            bulls += 1
+
+    for char in range(4):
+        if guess_numb[char] in NUMB and guess_numb[char] != NUMB[char]:
+            cows += 1
+
+    print(f'Bulles: {bulls},cows: {cows}')
 
     if bulls == 4:
         print('You win!')
-    else:
-        print('Try again:')
         break
--- hw8_task3.py
+++ hw8_task3.py
@@ -5,7 +5,7 @@ This code solves the second task from homework #8 "Statues"
 size_1 = [3, 1, 8, 5, 10]
 size_2 = []
 
-for i in range (0, max(size_1)):
+for i in range(0, max(size_1)):
     i += 1
     if i not in size_1:
         size_2.append(i)
