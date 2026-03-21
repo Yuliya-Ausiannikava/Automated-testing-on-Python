884124b4284b6481e59c8d373c6636c84f5b2286
Yuliya-Ausiannikava <ovsannikova.y@gmail.com>
2026-03-21 15:19:57 +0300

    Made corrections to the files according to the comments

--- hw8_task1.py
+++ hw8_task1.py
@@ -11,18 +11,16 @@ while True:
 
 while True:
     GUESS = str(input('The number has been chosen. Try to guess it: '))
-    if len(GUESS) != 4:
-        print('Please enter a 4 digit number:')
+    if len(set(GUESS)) != 4 or not GUESS.isdigit():
+        print('Please, enter a 4-digit number without repeating digits:')
         continue
 
     BULLS = 0
     COWS = 0
-    for char in range(4):
+    for char in range(len(GUESS)):
         if GUESS[char] == NUMBER[char]:
             BULLS += 1
-
-    for char in range(4):
-        if GUESS[char] in NUMBER and GUESS[char] != NUMBER[char]:
+        elif GUESS[char] in NUMBER and GUESS[char] != NUMBER[char]:
             COWS += 1
 
     print(f'Bulles: {BULLS},cows: {COWS}')
--- hw8_task2.py
+++ hw8_task2.py
@@ -4,6 +4,8 @@ This code solves the second task from homework #8 "Infinity loop"
 
 a = int(input("Enter the value of a: "))
 b = int(input("Enter the value of b: "))
+if a == b:
+    print(False)
 while a != b:
     a += 1
     b -= 1
