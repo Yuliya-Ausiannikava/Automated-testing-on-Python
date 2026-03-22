9586b63ce4315f755b497431dd911b56cf206717
Yuliya-Ausiannikava <ovsannikova.y@gmail.com>
2026-03-22 20:45:40 +0300

    Fixed linter errors

---
+++ hw9_numb_opposite.py
@@ -0,0 +1,23 @@
+"""
+This is the code to complete the "Sequence" task.
+"""
+
+
+def solution(n, f_number):
+
+    """
+    This function, given n and first_number, finds the number
+    that is written in the radially opposite position from first_number.
+    :arg n: integer n
+    :arg f_number: integer f_number
+    :return: integer opposite position from first_number
+    """
+    if n >= 0 and n % 2 == 0:
+        return (f_number + n // 2) % n
+    else:
+        return print("Error. n must be even and positive")
+
+
+assert solution(10, 6) == 1
+assert solution(10, 2) == 7
+assert solution(10, 4) == 9
--- hw9_subsequence.py
+++ hw9_subsequence.py
@@ -2,31 +2,36 @@
 This is the code to complete the "Sequence" task.
 """
 
+
 def solution(sequence):
-    def increase(list):
+
+    def check_increasing(arr):
         """
         This function checks if the numbers are increasing.
         arg list: list of numbers
         return: boolean
         """
-        for i in range(len(list) - 1):
-            if list[i] >= list[i + 1]:
+
+        for i in range(len(arr) - 1):
+            if arr[i] >= arr[i + 1]:
                 return False
         return True
 
     # Checking a list after excluding one element
     for item in range(len(sequence) - 1):
         if sequence[item] >= sequence[item + 1]:
-            without_item = sequence[:item] + sequence[item + 1:]
-            without_item_next = sequence[:item + 1] + sequence[item + 2:]
-            # We pass the lists obtained after removing one element as an argument to the function "increase"
-            if increase(without_item_next) or increase(without_item):
+            rm_item = sequence[:item] + sequence[item + 1:]
+            rm_next = sequence[:item + 1] + sequence[item + 2:]
+
+            # Checking for increment after deleting an element
+            if check_increasing(rm_next) or check_increasing(rm_item):
                 return True
             return False
     return True
 
-assert solution([1, 2, 3]) == True
-assert solution([1, 2, 1, 2]) == False
-assert solution([1, 3, 2, 1]) == False
-assert solution([1, 2, 3, 4, 5, 3, 5, 6]) == False
-assert solution([40, 50, 60, 10, 20, 30]) == False
+
+assert solution([1, 2, 3]) is True
+assert solution([1, 2, 1, 2]) is False
+assert solution([1, 3, 2, 1]) is False
+assert solution([1, 2, 3, 4, 5, 3, 5, 6]) is False
+assert solution([40, 50, 60, 10, 20, 30]) is False
