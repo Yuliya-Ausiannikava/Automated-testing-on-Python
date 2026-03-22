4dcce5b30cf6e7fb0446d8769f2ff436e94cbe72
Yuliya-Ausiannikava <ovsannikova.y@gmail.com>
2026-03-22 11:46:06 +0300

    Adjustments have been made to the code for Infinity loop

--- hw8_task2.py
+++ hw8_task2.py
@@ -4,13 +4,16 @@ This code solves the second task from homework #8 "Infinity loop"
 
 a = int(input("Enter the value of a: "))
 b = int(input("Enter the value of b: "))
+
 if a == b:
     print(False)
-while a != b:
-    a += 1
-    b -= 1
-    if a == b:
-        print(False)
-    if a > b:
-        print(True)
-        break
+else:
+    while a != b:
+        a += 1
+        b -= 1
+        if a == b:
+            print(False)
+            break
+        if a > b:
+            print(True)
+            break
