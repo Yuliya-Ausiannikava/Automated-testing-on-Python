c3b8e664cb1f66c6ecca773ba580c7e4f5c9801f
Yuliya-Ausiannikava <ovsannikova.y@gmail.com>
2026-03-30 21:21:53 +0300

    A decorator is created that checks if the result of a function is a number

---
+++ hw11_task2.py
@@ -0,0 +1,21 @@
+"""
+A decorator is created that checks if the result of a function is a number
+"""
+
+
+def res_number(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if not isinstance(result, (int, float)):
+            print("Error. The result of a function can only be a number.")
+            return None
+        return result
+    return wrapper
+
+
+@res_number
+def my_func(arg1, arg2):
+    return arg1 + arg2
+
+
+print(my_func('10', '2.5'))
