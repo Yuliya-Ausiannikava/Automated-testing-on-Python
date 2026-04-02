7fe0a47673143c6e81242a247f8d240168eff320
Yuliya-Ausiannikava <ovsannikova.y@gmail.com>
2026-04-02 17:08:51 +0300

    A program to assist the library was written. Two classes were created

---
+++ hw12_library.py
@@ -0,0 +1,80 @@
+"""
+A program to assist the library was written. Two classes were created.
+"""
+
+
+class Book:
+
+    def __init__(self, book_name, author, num_pages, isbn):
+        self.is_reserved_book = None
+        self.is_get_book = None
+        self.book_name = book_name
+        self.author = author
+        self.num_pages = num_pages
+        self.isbn = isbn
+
+    def reserve(self, reader_name):
+        if self.is_reserved_book is None and self.is_get_book is None:
+            self.is_reserved_book = reader_name
+            print(f"The book '{self.book_name}' has been reserved by the user {reader_name.name}")
+        else:
+            print('User can not reserve a book')
+
+    def cancel_reserve(self, reader_name):
+        if self.is_reserved_book == reader_name:
+            self.is_reserved_book = None
+            print(f"The book '{self.book_name}' has been cancelled by the user {reader_name.name}")
+        else:
+            print(f"{reader_name.name} can not cancel a book, because he didn't reserve it")
+
+    def get_book(self, reader_name):
+        if (self.is_reserved_book == reader_name or
+                (self.is_get_book is None and self.is_reserved_book is None)):
+            self.is_get_book = reader_name
+            print(f"The book '{self.book_name}' was taken by reader {reader_name.name}")
+        else:
+            print(f"{reader_name.name} can not get a book '{self.book_name}'")
+
+    def return_book(self, reader_name):
+        if self.is_get_book == reader_name:
+            self.is_get_book = None
+        else:
+            print(f"{reader_name.name} can not return a book '{self.book_name}', "
+                  "because he didn't take it")
+
+
+class Reader:
+
+    def __init__(self, name):
+        self.name = name
+
+    def reserve_book(self, book):
+        book.reserve(self)
+
+    def cancel_reserve(self, book):
+        book.cancel_reserve(self)
+
+    def get_book(self, book):
+        book.get_book(self)
+
+    def return_book(self, book):
+        book.return_book(self)
+
+
+book1 = Book(book_name="The Hobbit", author="Books by J.R.R. Tolkien",
+             num_pages=400, isbn="0006754023")
+book2 = Book(book_name="A Good Year", author="Peter Mayle", num_pages=320, isbn="9785389173118")
+book3 = Book(book_name="Wuthering Heights", author="Emily Jane Brontë",
+             num_pages=416, isbn="9785171272029")
+
+VASYA = Reader("Vasya")
+PETYA = Reader("Petya")
+
+VASYA.reserve_book(book1)
+PETYA.reserve_book(book1)
+
+VASYA.cancel_reserve(book1)
+PETYA.reserve_book(book1)
+
+VASYA.get_book(book1)
+VASYA.return_book(book2)
