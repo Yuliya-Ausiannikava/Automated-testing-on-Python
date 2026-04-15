8d88ce57a9b00950ef5a8c802bd0a6638b01e5a9
Yuliya-Ausiannikava <ovsannikova.y@gmail.com>
2026-04-15 19:55:47 +0300

    Wrapped the datetime tasks in functions

--- hw14_dt.py
+++ hw14_dt.py
@@ -5,22 +5,50 @@ Programs using the module datetime
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-# Counts the number of days between dates
-date_1 = input('Enter the first date in the format YYYY-MM-DD: ')
-date_2 = input('Enter the second date in the format YYYY-MM-DD: ')
-format_date_1 = datetime.strptime(date_1, "%Y-%m-%d").date()
-format_date_2 = datetime.strptime(date_2, "%Y-%m-%d").date()
-result = relativedelta(format_date_2, format_date_1)
-print(result)
+
+def count_days(date_1='', date_2=''):
+    """
+    Counts the number of days between dates
+    :param date_1: string in format YYYY-MM-DD
+    :param date_2: string in format YYYY-MM-DD
+    :return: relativedelta object
+    """
+    try:
+        format_date_1 = datetime.strptime(date_1, "%Y-%m-%d").date()
+        format_date_2 = datetime.strptime(date_2, "%Y-%m-%d").date()
+        result = relativedelta(format_date_2, format_date_1)
+        print(result)
+        return result
+    except ValueError:
+        print('Error: Invalid date format. Please use format YYYY-MM-DD')
+        return None
+
+
+count_days('2025-09-05', '2025-09-16')
 print('___________________________________________________________')
 
-# Checks whether a date is past or future
-date = input("Enter a date in the format YYYY-MM-DD: ")
-format_date = datetime.strptime(date, "%Y-%m-%d").date()
-today = datetime.today().date()
-if today > format_date:
-    print("The date entered is in the past")
-elif today < format_date:
-    print("The date entered is in the future")
-else:
-    print("The date entered is today")
+
+def past_future(date=''):
+    """
+    Checks whether a date is past or future
+    :param date: string in format YYYY-MM-DD
+    :return: string
+    """
+    try:
+        format_date = datetime.strptime(date, "%Y-%m-%d").date()
+        today = datetime.today().date()
+        result = ''
+        if today > format_date:
+            result = "The date entered is in the past"
+        elif today < format_date:
+            result = "The date entered is in the future"
+        else:
+            result = "The date entered is today"
+        print(result)
+        return result
+    except ValueError:
+        print('Error: Invalid date format. Please use format YYYY-MM-DD')
+        return None
+
+
+past_future('2024-04-05')
