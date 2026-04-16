9985b6429d38258f8443b007b5776f5ef3e63a9e
Yuliya-Ausiannikava <ovsannikova.y@gmail.com>
2026-04-16 19:32:54 +0300

    The second task of the test has been completed

---
+++ task2.py
@@ -0,0 +1,19 @@
+# The function asks for a number and prints the square
+def square(x):
+    return x ** 2
+
+
+result = square(2)
+print(result)
+
+
+# The function determines whether a number is even or odd
+def number(y):
+    if y % 2 == 0:
+        return "Number even"
+    else:
+        return "Number odd"
+
+
+result = number(2)
+print(result)
